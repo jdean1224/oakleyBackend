ed40b366f408163e5835567dd0bb1c7e45aa75b2
Justin Koski <jdeankoski@gmail.com>
2023-09-26 13:08:40 -0400

    Fixed bug4

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = deanjustin81;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
