8a6874e08b29ac8542814b0bcbf6ac389120ed0d
Justin Koski <jdeankoski@gmail.com>
2023-09-26 13:21:32 -0400

    Fixed bug5

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const io = new WebSocket.Server({ noServer: true });
+global.io = new WebSocket.Server({ noServer: true });
 const mongoose = require('mongoose');
 
 const dotenv = require('dotenv');
